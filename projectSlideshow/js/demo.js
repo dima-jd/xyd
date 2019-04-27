fffe19262dd63323035a20d16bb0e6b6c510dbfd
dima-jd <iakovytskyi@yahoo.com>
2019-04-27 23:29:58 +0300

    Update demo.js

--- projectSlideshow/js/demo.js
+++ projectSlideshow/js/demo.js
@@ -41,49 +41,49 @@
     // Recalculate window sizes on resize.
     window.addEventListener('resize', calcWinsize);
 
-    // Custom mouse cursor.
-    class CursorFx {
-        constructor(el) {
-            this.DOM = {el: el};
-            this.DOM.dot = this.DOM.el.querySelector('.cursor__inner--dot');
-            this.DOM.circle = this.DOM.el.querySelector('.cursor__inner--circle');
-            this.bounds = {dot: this.DOM.dot.getBoundingClientRect(), circle: this.DOM.circle.getBoundingClientRect()};
-            this.scale = 1;
-            this.opacity = 1;
-            this.mousePos = {x:0, y:0};
-            this.lastMousePos = {dot: {x:0, y:0}, circle: {x:0, y:0}};
-            this.lastScale = 1;
-            this.lastOpacity = 1;
+    // // Custom mouse cursor.
+    // class CursorFx {
+    //     constructor(el) {
+    //         this.DOM = {el: el};
+    //         this.DOM.dot = this.DOM.el.querySelector('.cursor__inner--dot');
+    //         this.DOM.circle = this.DOM.el.querySelector('.cursor__inner--circle');
+    //         this.bounds = {dot: this.DOM.dot.getBoundingClientRect(), circle: this.DOM.circle.getBoundingClientRect()};
+    //         this.scale = 1;
+    //         this.opacity = 1;
+    //         this.mousePos = {x:0, y:0};
+    //         this.lastMousePos = {dot: {x:0, y:0}, circle: {x:0, y:0}};
+    //         this.lastScale = 1;
+    //         this.lastOpacity = 1;
             
-            this.initEvents();
-            requestAnimationFrame(() => this.render());
-        }
-        initEvents() {
-            window.addEventListener('mousemove', ev => this.mousePos = getMousePos(ev));
-        }
-        render() {
-            this.lastMousePos.dot.x = lerp(this.lastMousePos.dot.x, this.mousePos.x - this.bounds.dot.width/2, 1);
-            this.lastMousePos.dot.y = lerp(this.lastMousePos.dot.y, this.mousePos.y - this.bounds.dot.height/2, 1);
-            this.lastMousePos.circle.x = lerp(this.lastMousePos.circle.x, this.mousePos.x - this.bounds.circle.width/2, 0.15);
-            this.lastMousePos.circle.y = lerp(this.lastMousePos.circle.y, this.mousePos.y - this.bounds.circle.height/2, 0.15);
-            this.lastScale = lerp(this.lastScale, this.scale, 0.15);
-            this.lastOpacity = lerp(this.lastOpacity, this.opacity, 0.1);
-            this.DOM.dot.style.transform = `translateX(${(this.lastMousePos.dot.x)}px) translateY(${this.lastMousePos.dot.y}px)`;
-            this.DOM.circle.style.transform = `translateX(${(this.lastMousePos.circle.x)}px) translateY(${this.lastMousePos.circle.y}px) scale(${this.lastScale})`;
-            this.DOM.circle.style.opacity = this.lastOpacity
-            requestAnimationFrame(() => this.render());
-        }
-        enter() {
-            cursor.scale = 2.7;
-        }
-        leave() {
-            cursor.scale = 1;
-        }
-        click() {
-            this.lastScale = 1;
-            this.lastOpacity = 0;
-        }
-    }
+    //         this.initEvents();
+    //         requestAnimationFrame(() => this.render());
+    //     }
+    //     initEvents() {
+    //         window.addEventListener('mousemove', ev => this.mousePos = getMousePos(ev));
+    //     }
+    //     render() {
+    //         this.lastMousePos.dot.x = lerp(this.lastMousePos.dot.x, this.mousePos.x - this.bounds.dot.width/2, 1);
+    //         this.lastMousePos.dot.y = lerp(this.lastMousePos.dot.y, this.mousePos.y - this.bounds.dot.height/2, 1);
+    //         this.lastMousePos.circle.x = lerp(this.lastMousePos.circle.x, this.mousePos.x - this.bounds.circle.width/2, 0.15);
+    //         this.lastMousePos.circle.y = lerp(this.lastMousePos.circle.y, this.mousePos.y - this.bounds.circle.height/2, 0.15);
+    //         this.lastScale = lerp(this.lastScale, this.scale, 0.15);
+    //         this.lastOpacity = lerp(this.lastOpacity, this.opacity, 0.1);
+    //         this.DOM.dot.style.transform = `translateX(${(this.lastMousePos.dot.x)}px) translateY(${this.lastMousePos.dot.y}px)`;
+    //         this.DOM.circle.style.transform = `translateX(${(this.lastMousePos.circle.x)}px) translateY(${this.lastMousePos.circle.y}px) scale(${this.lastScale})`;
+    //         this.DOM.circle.style.opacity = this.lastOpacity
+    //         requestAnimationFrame(() => this.render());
+    //     }
+    //     enter() {
+    //         cursor.scale = 2.7;
+    //     }
+    //     leave() {
+    //         cursor.scale = 1;
+    //     }
+    //     click() {
+    //         this.lastScale = 1;
+    //         this.lastOpacity = 0;
+    //     }
+    // }
     
     // Tilt effect used for the main slide's image and title.
     class TiltFx {
@@ -519,17 +519,17 @@
         }
     }
 
-    const cursor = new CursorFx(document.querySelector('.cursor'));
+    // const cursor = new CursorFx(document.querySelector('.cursor'));
     const slideshow = new Slideshow(document.querySelector('.slideshow'));
     const nav = new Navigation(document.querySelector('.nav'));
     slideshow.nav = nav;
 
-    // Custom cursor chnages state when hovering on elements with 'data-hover'.
-    [...document.querySelectorAll('[data-hover]')].forEach((link) => {
-        link.addEventListener('mouseenter', () => cursor.enter() );
-        link.addEventListener('mouseleave', () => cursor.leave() );
-        link.addEventListener('click', () => cursor.click() );
-    });
+    // // Custom cursor chnages state when hovering on elements with 'data-hover'.
+    // [...document.querySelectorAll('[data-hover]')].forEach((link) => {
+    //     link.addEventListener('mouseenter', () => cursor.enter() );
+    //     link.addEventListener('mouseleave', () => cursor.leave() );
+    //     link.addEventListener('click', () => cursor.click() );
+    // });
 
     // Preload all the images in the page.
     imagesLoaded(document.querySelectorAll('.slide__figure-img'), {background: true}, () => body.classList.remove('loading'));
